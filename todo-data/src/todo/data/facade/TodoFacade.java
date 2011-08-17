5f18855b015ba7889e567074bb4cf5cd7e5dc9f4
Marcus Bendtsen <marcus@alexit.se>
2011-08-17 12:54:36 +0200

    Find and JPQL

--- todo-data/src/todo/data/facade/TodoFacade.java
+++ todo-data/src/todo/data/facade/TodoFacade.java
@@ -1,6 +1,9 @@
 package todo.data.facade;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 import todo.data.entity.Todo;
 import todo.data.util.EMF;
@@ -38,6 +41,64 @@ public class TodoFacade {
 
 		}
 
+	}
+
+	public Todo find(long id) {
+
+		EntityManager entityManager = EMF.createEntityManager();
+
+		try {
+
+			return entityManager.find(Todo.class, id);
+
+		} catch (Exception e) {
+			return null;
+		} finally {
+
+			entityManager.close();
+
+		}
+
+	}
+
+	public List<Todo> findAll() {
+
+		EntityManager entityManager = EMF.createEntityManager();
+
+		try {
+
+			Query query = entityManager.createQuery("SELECT t FROM Todo t");
+
+			return query.getResultList();
+
+		} catch (Exception e) {
+			return null;
+		} finally {
+
+			entityManager.close();
+
+		}
+	}
+
+	public List<Todo> findByName(String name) {
+
+		EntityManager entityManager = EMF.createEntityManager();
+
+		try {
+
+			Query query = entityManager
+					.createQuery("SELECT t FROM Todo t WHERE t.name = :name");
+			query.setParameter("name", name);
+
+			return query.getResultList();
+
+		} catch (Exception e) {
+			return null;
+		} finally {
+
+			entityManager.close();
+
+		}
 	}
 
 }
--- todo-data/test/todo/data/facade/TodoFacadeTest.java
+++ todo-data/test/todo/data/facade/TodoFacadeTest.java
@@ -1,10 +1,13 @@
 package todo.data.facade;
 
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import todo.data.entity.Todo;
 import todo.data.util.EMF;
 
 public class TodoFacadeTest {
@@ -23,10 +26,37 @@ public class TodoFacadeTest {
 
 	@Test
 	public void testCreate() {
-
 		long id = todoFacade.create("Todo Item");
 		Assert.assertTrue(id != 0);
+	}
 
+	@Test
+	public void testFind() {
+		long id = todoFacade.create("Todo Item");
+
+		Todo todo = todoFacade.find(id);
+
+		Assert.assertEquals(id, todo.getId());
+		Assert.assertEquals("Todo Item", todo.getName());
+	}
+
+	@Test
+	public void testFindAll() {
+		long id = todoFacade.create("Todo Item");
+
+		List<Todo> todos = todoFacade.findAll();
+
+		Assert.assertTrue(todos.size() > 0);
+	}
+
+	@Test
+	public void testFindByName() {
+		long id = todoFacade.create("Todo Item");
+
+		Todo todo = todoFacade.findByName("Todo Item").get(0);
+
+		Assert.assertEquals(id, todo.getId());
+		Assert.assertEquals("Todo Item", todo.getName());
 	}
 
 }
