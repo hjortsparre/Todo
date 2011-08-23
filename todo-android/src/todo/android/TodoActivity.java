2443eb28cb1e6e2ede380d067c9c6ac62ee0fc98
marcus-bendtsen <marcus@alexit.se>
2011-08-23 12:10:57 +0200

    Print all items to screen

--- todo-android/src/todo/android/TodoActivity.java
+++ todo-android/src/todo/android/TodoActivity.java
@@ -9,20 +9,43 @@ import todo.android.webservice.TodoDTO;
 import todo.android.webservice.WebServiceRequest;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 public class TodoActivity extends Activity {
-    
-	
-	/** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
 
-		
-    }
+	private Gson gson = new Gson();
+	private WebServiceRequest webServiceRequest = new WebServiceRequest();
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+
+		ResponseDTO responseDTO = webServiceRequest.list();
+
+		System.out.println(responseDTO);
+
+		Type collectionType = new TypeToken<Collection<TodoDTO>>() {
+		}.getType();
+		List<TodoDTO> todos = gson.fromJson(responseDTO.getPayload(),
+				collectionType);
+
+		LinearLayout linearLayout = new LinearLayout(this);
+		linearLayout.setOrientation(LinearLayout.VERTICAL);
+		ListView listView = new ListView(this);
+		for (TodoDTO todo : todos) {
+			TextView textView = new TextView(this);
+			textView.setText(todo.getName());
+			linearLayout.addView(textView);
+		}
+
+		setContentView(linearLayout);
+	}
+
 }
